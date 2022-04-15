9019fb071a00936a63a3dfcc02c9aa8dc2d161fe
nakazawaken1 <nakazawaken1@gmail.com>
2022-04-16 00:24:45 +0900

    #14 ラベル対応

--- zircon.mjs
+++ zircon.mjs
@@ -4,8 +4,10 @@ program: {space} expression {{space} expression} {space}
 expression: term {{space} ('+'|'-') {space} term}
 term: sign {{space} ('*'|'/'|'%') {space} sign}
 sign: {'-'|'+'} {space} factor
-factor: number | '(' {space} expression {space} ')' | 'p' expression
+factor: number | '(' {space} expression {space} ')' | 'p' expression | symbol | symbol ':' expression
+symbol: ^number|mark|space^ {symbol|number}
 number: ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9') {number}
+mark: ('!' | '"' | '#' | '$' | '%' | '&' | '\'' | '(' | ')' | '-' | '=' | '^' | '~' | '\\' | '|' | '@' | '`' | '[' | '{' | ';' | '+' | '*' | ']' | '}' | ',' | '<' | '.' | '>' | '/' | '?' | '_') {mark}
 space: (blank|newline) {space}
 blank: (' ' | '　' | '\t') {blank}
 newline: ('\n' | ';') {newline}
@@ -16,6 +18,8 @@ export default {
     const NEWLINE = ["\n", ';']
     const SPACE = BLANK.concat(NEWLINE)
     const DIGIT = '0123456789'.split('')
+    const MARK = ['!', '"', '#', '$', '%', '&', "\'", '(', ')', '-', '=', '^', '~', "\\", ',', '@', '`', '[', '{', ';', '+', '*', ']', '}', ',', '<', '.', '>', '/', '?', '_']
+    const DIGIT_MARK_SPACE = DIGIT.concat(MARK).concat(SPACE)
     const ADD = ['+', '-']
     const MUL = ['*', '/', '%']
     let position = 0
@@ -39,11 +43,12 @@ export default {
     function next() {
       position++
     }
-    function skip(letters = SPACE, ifEmpty = null) {
+    function skip(letters = SPACE, ifEmpty = null, not = false) {
       const result = []
       while (true) {
         const letter = peek()
-        if (!letters.includes(letter)) break
+        if (not) { if (letter == '\0' || letters.includes(letter)) break }
+        else { if (!letters.includes(letter)) break }
         result.push(letter)
         next()
       }
@@ -101,7 +106,8 @@ export default {
       }
     }
     function factor() {
-      switch (peek()) {
+      const letter = peek()
+      switch (letter) {
         case 'p':
           next()
           skip()
@@ -115,16 +121,27 @@ export default {
           next()
           return log(value, 'factor')
         default:
-          return log(number(), 'factor')
+          const n = number(null)
+          if (n !== '') return log(n, 'factor')
+          const name = symbol()
+          skip()
+          if (peek() != ':') return log(['get', name], 'factor')
+          next()
+          skip()
+          return log(['set', name, expression()], 'factor')
       }
     }
-    function number() {
-      const n = skip(DIGIT, '数値が見つかりません')
-      return log(Number(n), 'number')
+    function symbol(ifEmpty = 'シンボルが見つかりません') {
+      const s = skip(DIGIT_MARK_SPACE, ifEmpty, true)
+      return log(s, 'symbol')
+    }
+    function number(ifEmpty = '数値が見つかりません') {
+      const n = skip(DIGIT, ifEmpty)
+      return log(n.length ? Number(n) : n, 'number')
     }
   },
   run(ast, env) {
-    const run_ = i => this.run(i, env)
+    const r = i => this.run(i, env)
     function log(value) {
       console.log(JSON.stringify(ast) + ' = ' + JSON.stringify(value))
       return value
@@ -132,27 +149,39 @@ export default {
     if (ast instanceof Array) {
       switch (ast[0]) {
         case '+':
-          return log(ast.length < 3 ? +run_(ast[1]) : run_(ast[1]) + run_(ast[2]))
+          return log(ast.length < 3 ? +r(ast[1]) : r(ast[1]) + r(ast[2]))
         case '-':
-          return log(ast.length < 3 ? -run_(ast[1]) : run_(ast[1]) - run_(ast[2]))
+          return log(ast.length < 3 ? -r(ast[1]) : r(ast[1]) - r(ast[2]))
         case '*':
-          return log(run_(ast[1]) * run_(ast[2]))
+          return log(r(ast[1]) * r(ast[2]))
         case '/':
         case '%':
-          const right = run_(ast[2])
+          const right = r(ast[2])
           if (right == 0) throw '0で除算できません'
-          const left = run_(ast[1])
+          const left = r(ast[1])
           return log(ast[0] == '%' ? left % right : left / right)
-        case 'p':
-          const value = run_(ast[1])
+        case 'p': {
+          const value = r(ast[1])
           env.out(JSON.stringify(value) + "\n")
           return log(value)
-        case 'run':
+        }
+        case 'run': {
           let result
           for (const i of ast.slice(1)) {
-            result = run_(i)
+            result = r(i)
           }
           return log(result)
+        }
+        case 'get': {
+          const name = r(ast[1])
+          if (!(name in env)) throw name + 'は定義されていません'
+          return log(env[name])
+        }
+        case 'set': {
+          const value = r(ast[2])
+          env[r(ast[1])] = value
+          return log(value)
+        }
         default:
           throw ast[0] + 'は定義されていません'
       }
