d522fefbd016412fa710c8a67810f240b3704dca
nakazawaken1 <nakazawaken1@gmail.com>
2022-05-07 01:39:43 +0900

    型関数対応(type, ., ())

--- zircon.mjs
+++ zircon.mjs
@@ -1,22 +1,25 @@
 /*
 [BNF] {} 0回以上, [] 0または1回, | または, ^ それ以外
-program: [space] [expression] {space expression} [space]
-expression: prefix [space] factor
-  | factor {[space] operator [space] factor}
+program: [space] [call] {space call} [space]
+call: expression {blank expression} [do]
+expression: prefix [blank] factor
+  | factor {[blank] operator [blank] factor}
 factor: number
   | tuple
-  | '?' [space] expression
+  | '?' [space] call
   | '\'' {^'\''^|'\'\''} '\''
   | symbol
   | symbol [space] tuple
-  | symbol [space] ':' [space] expression
-  | 'do' [space] [argument] [space] block
-  | 'if' [space] expression [space] block {[space] 'ef' [space] expression [space] block} [[space] 'else' [space] block]
+  | symbol [space] ':' [space] call
+  | do
+  | if
+do: 'do' [space] [argument] [space] block
+if: 'if' [space] call [space] block {[space] 'ef' [space] call [space] block} [[space] 'else' [space] block]
 prefix: '+' | '-' | 'not'
 operator: '+' | '-' | '*' | '/' | '%' | '<' | '<=' | '=' | '<>' | '>=' | '>' | 'and' | 'or' | '^' | '??' | '&' | '??&' | '.' | '?.' | '..' | '|.' | '|?.'
 argument: symbol {(space|',') [space] symbol}
 block: '{' program '}'
-tuple: '(' [space] [expression] {(space|',') [space] expression} [space] ')'
+tuple: '(' [space] [call] {(space|',') [space] call} [space] ')'
 symbol: 'true' | 'false' | 'null' | (^number|mark|space^ [symbol|number])
 number: digit ['.' digit]
 digit: ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9') [digit]
@@ -26,16 +29,8 @@ blank: (' ' | '　' | '\t') [blank]
 newline: ('\n' | ';') [newline]
 */
 const toJSON = (a, space = '  ') => JSON.stringify(a, null, space)
+const toString = a => a[0] == 'tuple' ? toJSON(a.slice(1), null) : a
 const isNumber = (value) => typeof value == 'number' && isFinite(value)
-const type = a => {
-  if (a == null) return 'Null'
-  if (a === true || a === false) return 'Boolean'
-  if (a[0] == 'tuple') return 'Tuple'
-  if (a[0] == 'do') return 'Do'
-  if (typeof a == 'string') return 'String'
-  if (isNumber(a)) return String(a).includes('.') ? 'Real' : 'Integer'
-  return 'Any'
-}
 export default {
   toJSON,
   parse(source) {
@@ -119,10 +114,32 @@ export default {
       while (true) {
         skip()
         if ("\0}".includes(peek())) break
-        result.push(expression(env))
+        result.push(call(env))
       }
       skip()
-      return result
+      return log(result, 'program')
+    }
+    function call(env) {
+      const queue = [expression(env)]
+      while (true) {
+        skip(BLANK)
+        const backup = position
+        try {
+          const a = [expression(env)]
+          const d = do_(env)
+          if (d) a.push(d)
+          queue.unshift(a)
+        } catch {
+          position = backup
+          break
+        }
+      }
+      if (queue.length <= 1) return log(queue[0], 'call')
+      return log(queue.reduce(([p, d], c) => {
+        const argument = p && p[0] == 'tuple' ? p.slice(1) : [p]
+        if (d) argument.push(d)
+        return ['call', c, argument]
+      }), 'call')
     }
     function expression(env) {
       const stack = []
@@ -134,13 +151,13 @@ export default {
           const top = stack[stack.length - 1]
           if (stack.length && top[1] > unary[1]) {
             position = backup
-            right = expression(env)
+            right = call(env)
             break
           }
           stack.push(unary)
           continue
         }
-        skip()
+        skip(BLANK)
         right = factor(env)
         const binary = eatOperator(i => !'\f\b'.includes(i[2]))
         if (binary == null) break
@@ -172,7 +189,7 @@ export default {
       return log(right, 'expression')
 
       function eatOperator(filter = i => true) {
-        skip()
+        skip(BLANK)
         const backup = position
         return OPERATOR.find(i => {
           if (eat(i[0]) && filter(i)) return true
@@ -186,7 +203,7 @@ export default {
         case '?':
           next()
           skip()
-          return log(['?', expression(env)], 'factor')
+          return log(['?', call(env)], 'factor')
         case '(':
           const value = tuple(env)
           return log(value.length == 2 ? value[1] : value, 'factor')
@@ -205,24 +222,19 @@ export default {
           eat('\'', true)
           return log(letters.join(''), 'factor')
         }
-        default:
-          if (eat('do')) {
-            skip()
-            const a = peek() != '{' ? argument(env) : []
-            skip()
-            const b = block(env)
-            return log(['do', a, b], 'factor')
-          }
+        default: {
+          const d = do_(env)
+          if (d) return log(d, 'factor')
           if (eat('if')) {
             const ifs = ['if']
             skip()
-            ifs.push(expression(env))
+            ifs.push(call(env))
             skip()
             ifs.push(block(env))
             skip()
             while (eat('ef')) {
               skip()
-              ifs.push(expression(env))
+              ifs.push(call(env))
               skip()
               ifs.push(block(env))
             }
@@ -236,19 +248,28 @@ export default {
           const n = number(null)
           if (n !== '') return log(n, 'factor')
           const name = symbol()
-          skip()
+          skip(BLANK)
           switch (peek()) {
             case ':':
               next()
-              skip()
-              return log(['set', name, expression(env)], 'factor')
+              skip(BLANK)
+              return log(['set', name, call(env)], 'factor')
             case '(':
               const argument = tuple(env).slice(1)
               return log(['call', ['get', name], argument], 'factor')
             default:
               return log(['get', name], 'factor')
           }
+        }
       }
+    }
+    function do_(env) {
+      if (!eat('do')) return null
+      skip()
+      const a = peek() != '{' ? argument(env) : []
+      skip()
+      const b = block(env)
+      return log(['do', a, b], 'do')
     }
     function argument(env) {
       const symbols = [symbol()]
@@ -271,7 +292,7 @@ export default {
       skip()
       const result = ['tuple']
       while (peek() != ')') {
-        result.push(expression(env))
+        result.push(call(env))
         skip()
         eat(',')
         skip()
@@ -294,165 +315,183 @@ export default {
     }
   },
   run(ast, env = {}) {
+    const type = a => {
+      if (a == null) return 'Null'
+      if (a === true || a === false) return 'Boolean'
+      if (a[0] == 'tuple') return 'Tuple'
+      if (a[0] == 'range') return 'Range'
+      if (a[0] == 'do') return 'Do'
+      if (typeof a == 'string') return 'String'
+      if (isNumber(a)) return String(a).includes('.') ? 'Real' : 'Integer'
+      return 'Any'
+    }
     env.$ = {
       type,
-      echo: a => env.out(a)
+      print: a => env.out(toString(a)),
+      echo: a => env.out(toString(a) + '\n')
     }
     ast[1].$ = env
-    const result = this.eval(ast, ast[1])
+    const result = evaluate(ast, ast[1])
     return result && result[0] == 'tuple' ? result.slice(1) : result
-  },
-  eval(ast, env) {
-    if (ast instanceof Array) {
-      const r = i => this.eval(i, env)
-      switch (ast[0]) {
-        case '+':
-          return log(ast.length < 3 ? +r(ast[1]) : r(ast[1]) + r(ast[2]))
-        case '-':
-          return log(ast.length < 3 ? -r(ast[1]) : r(ast[1]) - r(ast[2]))
-        case '*':
-          return log(r(ast[1]) * r(ast[2]))
-        case '/':
-        case '%':
-        case '/%':
-          const right = r(ast[2])
-          if (right == 0) throw '0で除算できません'
-          const left = r(ast[1])
-          switch (ast[0]) {
-            case '/': return log(left / right)
-            case '%': return log(left % right)
-            case '/%': return log(Math.floor(left / right))
-          }
-        case '^':
-          return log(Math.pow(r(ast[1]), r(ast[2])))
-        case '<':
-          return log(r(ast[1]) < r(ast[2]))
-        case '>':
-          return log(r(ast[1]) > r(ast[2]))
-        case '<=':
-          return log(r(ast[1]) <= r(ast[2]))
-        case '>=':
-          return log(r(ast[1]) >= r(ast[2]))
-        case '=':
-          return log(r(ast[1]) === r(ast[2]))
-        case '<>':
-          return log(r(ast[1]) !== r(ast[2]))
-        case 'and':
-          return log(r(ast[1]) && r(ast[2]))
-        case 'or':
-          return log(r(ast[1]) || r(ast[2]))
-        case 'not':
-          return log(!r(ast[1]))
-        case '?': {
-          const value = r(ast[1])
-          for (var e = env; e != null; e = e.$) {
-            if ('out' in e) {
-              e.out((value[0] == 'tuple' ? toJSON(value.slice(1), null) : value) + "\n")
-              return log(value)
-            }
-          }
-          throw '出力先(out)が定義されていません'
-        }
-        case '??': {
-          const value = r(ast[1])
-          return log(value == null ? r(ast[2]) : value)
-        }
-        case '??&': {
-          const value = r(ast[1])
-          return log(String(value === null || value === '' ? r(ast[2]) : value))
-        }
-        case '&':
-          return log(`${r(ast[1]) ?? ''}${r(ast[2]) ?? ''}`)
-        case '..':
-          return log(['range', r(ast[1]), r(ast[2])])
-        case 'run': {
-          let result = null
-          for (var i = 2; i < ast.length; i++) {
-            result = r(ast[i])
-            if (i + 1 < ast.length && ast[i + 1][0] == 'do') {
-              if (result[0] == 'range') {
-                for (var j = result[1], end = result[2]; j <= end; j++) {
-                  result = r(['call', ast[i + 1], [j]])
-                }
-                i++
+
+    function evaluate(ast, env) {
+      if (ast instanceof Array) {
+        const r = i => evaluate(i, env)
+        const types = {
+          Tuple: {
+            '.': (left, right) => left.slice(1)[right < 0 ? left.length - 1 + right : right] || null
+          },
+          Range: {
+            '()': (range, action) => {
+              for (var i = range[1], end = range[2]; i <= end; i++) {
+                r(['call', action, [i]])
               }
             }
           }
-          return log(result)
         }
-        case 'get': {
-          const name = r(ast[1])
-          if (name == 'true') {
-            return true
-          }
-          if (name == 'false') {
-            return false
-          }
-          if (name == 'null') {
-            return null
-          }
-          for (var e = env; e != null; e = e.$) {
-            if (name in e) return log(e[name])
-          }
-          throw name + 'は定義されていません'
-        }
-        case 'set': {
-          const value = r(ast[2])
-          env[r(ast[1])] = value
-          return log(value)
-        }
-        case 'call': {
-          const f = r(ast[1])
-          if (f instanceof Function) return log(f(...ast[2])) // 組み込み関数
-          const [_, argument, block] = f
-          const parameter = ast[2]
-          const local = block[1]
-          const max = Math.max(argument.length, parameter.length)
-          for (let i = 0; i < max; i++) {
-            console.log(argument[i] + ' <= ' + r(parameter[i]))
-            local[argument[i]] = r(parameter[i])
-          }
-          return this.eval(block, { ...local })
-        }
-        case 'tuple':
-          return log(['tuple', ...ast.slice(1).map(i => r(i))])
-        case 'do':
-          return ast
-        case 'if': {
-          const condition = r(ast[1])
-          if (condition) return log(r(ast[2]))
-          for (let i = 3; i < ast.length; i++) {
-            const v = ast[i++]
-            if (v[0] && v[0] == 'run') {
-              return log(r(v))
+        switch (ast[0]) {
+          case '+':
+            return log(ast.length < 3 ? +r(ast[1]) : r(ast[1]) + r(ast[2]))
+          case '-':
+            return log(ast.length < 3 ? -r(ast[1]) : r(ast[1]) - r(ast[2]))
+          case '*':
+            return log(r(ast[1]) * r(ast[2]))
+          case '/':
+          case '%':
+          case '/%': {
+            const right = r(ast[2])
+            if (right == 0) throw '0で除算できません'
+            const left = r(ast[1])
+            switch (ast[0]) {
+              case '/': return log(left / right)
+              case '%': return log(left % right)
+              case '/%': return log(Math.floor(left / right))
             }
-            if (r(v)) return log(r(ast[i]))
           }
-          return log(null)
+          case '^':
+            return log(Math.pow(r(ast[1]), r(ast[2])))
+          case '<':
+            return log(r(ast[1]) < r(ast[2]))
+          case '>':
+            return log(r(ast[1]) > r(ast[2]))
+          case '<=':
+            return log(r(ast[1]) <= r(ast[2]))
+          case '>=':
+            return log(r(ast[1]) >= r(ast[2]))
+          case '=':
+            return log(r(ast[1]) === r(ast[2]))
+          case '<>':
+            return log(r(ast[1]) !== r(ast[2]))
+          case 'and':
+            return log(r(ast[1]) && r(ast[2]))
+          case 'or':
+            return log(r(ast[1]) || r(ast[2]))
+          case 'not':
+            return log(!r(ast[1]))
+          case '?': {
+            const value = r(ast[1])
+            for (var e = env; e != null; e = e.$) {
+              if ('out' in e) {
+                e.out(toString(value) + "\n")
+                return log(value)
+              }
+            }
+            throw '出力先(out)が定義されていません'
+          }
+          case '??': {
+            const value = r(ast[1])
+            return log(value == null ? r(ast[2]) : value)
+          }
+          case '??&': {
+            const value = r(ast[1])
+            return log(String(value === null || value === '' ? r(ast[2]) : value))
+          }
+          case '&':
+            return log(`${r(ast[1]) ?? ''}${r(ast[2]) ?? ''}`)
+          case '..':
+            return log(['range', r(ast[1]), r(ast[2])])
+          case 'run': {
+            let result = null
+            for (var i = 2; i < ast.length; i++) result = r(ast[i])
+            return log(result)
+          }
+          case 'get': {
+            const name = r(ast[1])
+            if (name == 'true') {
+              return true
+            }
+            if (name == 'false') {
+              return false
+            }
+            if (name == 'null') {
+              return null
+            }
+            for (var e = env; e != null; e = e.$) {
+              if (name in e) return log(e[name])
+            }
+            throw name + 'は定義されていません'
+          }
+          case 'set': {
+            const value = r(ast[2])
+            env[r(ast[1])] = value
+            return log(value)
+          }
+          case 'call': {
+            const f = r(ast[1])
+            if (f instanceof Function) return log(f(...ast[2])) // 組み込み関数
+            const t = types[type(f)]
+            const apply = t && t['()']
+            if (apply) return log(r(['call', apply, [f, ...ast[2]]]))
+            if (f == null || f[0] != 'do') return log(r(ast[2][0]))
+            const [_, argument, block] = f
+            const parameter = ast[2]
+            const local = block[1]
+            const max = Math.max(argument.length, parameter.length)
+            for (let i = 0; i < max; i++) {
+              console.log(argument[i] + ' <= ' + r(parameter[i]))
+              local[argument[i]] = r(parameter[i])
+            }
+            return evaluate(block, { ...local })
+          }
+          case 'tuple':
+            return log(['tuple', ...ast.slice(1).map(i => r(i))])
+          case 'do':
+            return ast
+          case 'if': {
+            const condition = r(ast[1])
+            if (condition) return log(r(ast[2]))
+            for (let i = 3; i < ast.length; i++) {
+              const v = ast[i++]
+              if (v[0] && v[0] == 'run') {
+                return log(r(v))
+              }
+              if (r(v)) return log(r(ast[i]))
+            }
+            return log(null)
+          }
+          case '.':
+          case '?.':
+          case '|.':
+          case '|?.': {
+            const instance = r(ast[1])
+            if (instance == null && ast[0].endsWith('?.')) return log(null)
+            if (ast[2][0] == 'get') return log(r(['call', ast[2], [instance]]))
+            const message = r(ast[2])
+            const t = type(instance)
+            const f = types[t][ast[0]]
+            if (f == null) throw `${t}#${ast[0]}は定義されていません`
+            return log(r(['call', f, [instance, message]]))
+          }
+          default:
+            throw ast[0] + 'は定義されていません ' + toJSON(ast)
         }
-        case '.':
-        case '?.':
-        case '|.':
-        case '|?.': {
-          const instance = r(ast[1])
-          if (instance == null && ast[0].endsWith('?.')) return log(null)
-          if (ast[2][0] == 'get') return log(r(['call', ast[2], [instance]]))
-          const message = r(ast[2])
-          if (instance && instance[0] == 'tuple' && isNumber(message)) return log(then(instance.slice(1), a => a[(message < 0 ? a.length + message : message)] ?? null))
-          /*fallthrough*/
-        }
-        default:
-          throw ast[0] + 'は定義されていません ' + toJSON(ast)
       }
-    }
-    return ast
+      return ast
 
-    function log(value) {
-      console.log(toJSON(ast) + ' = ' + toJSON(value))
-      return value
-    }
-    function then(value, action) {
-      return action(value)
+      function log(value) {
+        console.log(toJSON(ast) + ' = ' + toJSON(value))
+        return value
+      }
     }
   }
 }
